ab3b72ea8e95da79210d898b148f00fa0e9e4e96
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-04-24 19:02:20 -0300

    fix: Fixed spelling mistakes in Post model and made migrations.

--- blog_project/mysite/blog/models.py
+++ blog_project/mysite/blog/models.py
@@ -12,18 +12,19 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, 
-                              ondelete=models.CASCADE, 
+                              on_delete=models.CASCADE, 
                               related_name='blog_posts')
     body = models.TextField()
+    # using datetime.now to get the current time (fuso horario).
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.charfield(max_length=10,
+    status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
 
     class Meta:
-        ordering = ('-publish')
+        ordering = ('-publish',)
 
     def __str__(self):
         return str(self.title)
