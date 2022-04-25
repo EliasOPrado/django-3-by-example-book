1e9cdc8d4881736512c8ac2b1deef1c59699f49f
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-04-24 22:09:42 -0300

    fix: fixed form inheritance.

--- blog_project/mysite/blog/forms.py
+++ blog_project/mysite/blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class EmailPost(forms.ModelForm):
+class EmailPost(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
