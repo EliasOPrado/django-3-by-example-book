ac2dbeeaa3007f522ecb58b99ab83ad74218108a
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-04-24 22:00:18 -0300

    chore: Updated view from function to class based view.

--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -1,24 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
-def post_list(request):
-    object_list = Post.objects_published.all()
-    paginator = Paginator(object_list, 3)# three post for each page.
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # if the page is not an integer, show the first page.
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if the page is being out of intervals, 
-        # shows the last result page.
-        posts = paginator.page(paginator.num_pages)
-        
-    context = {'page':page, 'posts':posts}
-    return render(request, 'blog/post/list.html', context)
+class PostListView(ListView):
+    queryset = Post.objects_published.all()
+    context_object_name = 'posts'
+    paginated_by = 3
+    template_name = 'blog/post/list.html'
+
 
 
 def post_detail(request, year, month, day, post):
