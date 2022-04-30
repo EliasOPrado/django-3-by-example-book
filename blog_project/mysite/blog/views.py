f5505dffff465acbd87bd26378cd52295f379dd3
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-04-30 18:17:32 -0300

    chore: Added comment functionality.

--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 
 # Create your views here.
 class PostListView(ListView):
@@ -12,15 +12,39 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-
 def post_detail(request, year, month, day, post):
     post = Post.objects.get(slug=post,
                              status='published', 
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    context = {'post':post}
-    return render(request, 'blog/post/detail.html', {'post':post})
+
+    # active comment list for posts.
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+
+        context = {
+            'post':post,
+            'comments':comments, 
+            'new_comment':new_comment, 
+            'comment_form':comment_form
+            }
+        return render(request, 'blog/post/detail.html', context)
 
 
 def post_share(request, post_id):
