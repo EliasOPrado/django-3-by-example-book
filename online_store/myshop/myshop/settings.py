1887b84beb5af80b599f31c5e16350a84e26e4f7
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-05-31 02:36:50 -0300

    chore: created Cart object.

--- online_store/myshop/myshop/settings.py
+++ online_store/myshop/myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -113,6 +114,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CART_SESSION_ID = 'cart'
 
 
 # Static files (CSS, JavaScript, Images)
