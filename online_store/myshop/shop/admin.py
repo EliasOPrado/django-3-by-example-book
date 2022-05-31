812235ad18841fcb3e5239f38f811a64f56be543
EliasOPrado <eliaspradoprofessional@outlook.com>
2022-05-31 01:30:03 -0300

    chore: added initial url config as well as views.

--- online_store/myshop/shop/admin.py
+++ online_store/myshop/shop/admin.py
@@ -5,6 +5,9 @@ from .models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["name", "slug"]
+
+    # will pre-populate the slug-field with-
+    # the name. Interesting.
     prepopulated_fields = {"slug":("name",)}
 
 @admin.register(Product)
@@ -12,5 +15,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ["name", "slug", "price",
                     "available", "created", "updated"]
     list_filter = ["available", "created", "updated"]
+
+    # will allow to edit values listed.
     list_editable = ["price", "available"]
     prepopulated_fields = {"slug":("name",)}
